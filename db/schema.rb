3e6066c16f06dc694092c804ed2b5b6f0bc39072
pcbennett108 <pcbennett42@gmail.com>
2023-07-10 16:19:37 -0600

    Add scheme and migrate Artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_024640) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_220920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "primary_media"
+    t.string "style"
+    t.boolean "published"
+    t.integer "exhibitions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "galleries", force: :cascade do |t|
     t.string "name"
