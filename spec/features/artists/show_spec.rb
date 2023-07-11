de66e0c8e7b3a082f96ac3c2520e527404b61673
pcbennett108 <pcbennett42@gmail.com>
2023-07-10 20:38:06 -0600

    Add test for artist includes key to gallery

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'the artists show page' do
   
   it 'displays the artist name' do
     gallery = Gallery.create!(name: 'Uptown Art', city: 'Denver', catering: true, viewing_rooms: 12)
-    artist = Artist.create!(name: "Don Plimpton", primary_media: "Oil", style: "Expressionism", published: false, exhibitions: 7)
+    artist = gallery.artists.create!(name: "Don Plimpton", primary_media: "Oil", style: "Expressionism", published: false, exhibitions: 7)
     artist_2 = gallery.artists.create!(name: "Sarah Meyers", primary_media: "Watercolor", style: "Realism", published: true, exhibitions: 21)
 
     visit "/artists/#{artist.id}"
@@ -15,12 +15,12 @@ RSpec.describe 'the artists show page' do
 
   it 'displays the name of hte artist of the song' do
     gallery = Gallery.create!(name: 'Uptown Art', city: 'Denver', catering: true, viewing_rooms: 12)
-    artist = Artist.create!(name: "Don Plimpton", primary_media: "Oil", style: "Expressionism", published: false, exhibitions: 7)
+    artist = gallery.artists.create!(name: "Don Plimpton", primary_media: "Oil", style: "Expressionism", published: false, exhibitions: 7)
     artist_2 = gallery.artists.create!(name: "Sarah Meyers", primary_media: "Watercolor", style: "Realism", published: true, exhibitions: 21)
 
     visit "/artists/#{artist.id}"
     #save_and_open_page
-    expect(page).to have_content(artist.name)
+    expect(page).to have_content(gallery.name)
   end
 
 
